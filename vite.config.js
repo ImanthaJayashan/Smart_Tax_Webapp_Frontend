b9ae6b87538a09b49c075aa07eedb90a9afd923a
Imantha Jayashan Galagedara <142011311+ImanthaJayashan@users.noreply.github.com>
2025-03-23 15:09:54 +0530

    config vite

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    port: 5173, // Fixed port
+    strictPort: true, // Prevents Vite from switching to a different port
+    host: 'localhost' // Ensures it binds to localhost
+  }
 })
+
+
